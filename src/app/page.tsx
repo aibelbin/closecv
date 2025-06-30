03690ef76971495dcce9497b85033ddd3c9998b0
aibelbin <aibelbinzacariah@gmail.com>
2025-07-01 00:20:45 +0530

    working project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+'use client';
+
+import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">    
-    
-    </div>
+    <main className="h-screen w-full bg-black text-white flex items-center justify-center">
+      <motion.h1
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        className="text-6xl font-bold"
+      >
+        Aibel Bin Zacariah
+      </motion.h1>
+    </main>
   );
 }
